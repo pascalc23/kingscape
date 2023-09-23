e6cddab4274c1356c28e2a5db9e3abb9a3edf9f8
Pascal Costa <mail@pascalcosta.com>
2023-09-23 15:05:26 +0200

    Add King

--- Assets/Scripts/Game/GridObjects/King.cs
+++ Assets/Scripts/Game/GridObjects/King.cs
@@ -2,10 +2,9 @@ using UnityEngine;
 
 namespace Assets.Scripts.Game.GridObjects
 {
-    public class Pawn : ActiveGridItem
+    public class King : ActiveGridItem
     {
         [SerializeField] private Vector2Int forwardVector;
-
         protected override void OnHeartbeat()
         {
             Move(forwardVector);
