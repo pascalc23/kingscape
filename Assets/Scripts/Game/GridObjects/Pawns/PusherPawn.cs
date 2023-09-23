48207300cf18f84c3eba30e1b437b78895af5fda
Pascal Costa <mail@pascalcosta.com>
2023-09-23 20:22:32 +0200

    remove comment

--- Assets/Scripts/Game/GridObjects/Pawns/PusherPawn.cs
+++ Assets/Scripts/Game/GridObjects/Pawns/PusherPawn.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 namespace Assets.Scripts.Game.GridObjects.Pawns
 {
     public class PusherPawn : MovingGridItem
@@ -7,8 +5,6 @@ namespace Assets.Scripts.Game.GridObjects.Pawns
         protected override void OnHalt()
         {
             base.OnHalt();
-            
-            Debug.Log("I PUSHER HALTED");
 
             // Check if item ahead is pushable - if so, push it and die
             if (gridManager.TryPush(GetNextCoordinate(), forwardVector))
