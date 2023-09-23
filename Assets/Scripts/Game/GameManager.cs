c468e17ca210bd9f4975e1949ec48c2fc8095523
Pascal Costa <mail@pascalcosta.com>
2023-09-23 16:32:36 +0200

    Move win/lose condition checks to King

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -11,12 +11,12 @@ namespace Assets.Scripts.Game
     {
         [SerializeField] private float timeBetweenHeartbeats = 1f;
 
+        public bool GameRunning { get; private set; }
+
         public UnityEvent<int> eventHeartbeat = new();
 
         private GridManager _gridManager;
-        private bool _gameRunning;
         private int _heartbeat;
-        private Vector2Int _previousKingPosition;
 
         private void Start()
         {
@@ -27,7 +27,7 @@ namespace Assets.Scripts.Game
         private void StartGame()
         {
             if (!_gridManager.IsLevelReady()) throw new Exception("Cannot start game - Level is not ready");
-            _gameRunning = true;
+            GameRunning = true;
             StartHeartbeat();
         }
 
@@ -39,7 +39,7 @@ namespace Assets.Scripts.Game
 
         private IEnumerator Heartbeat()
         {
-            while (_gameRunning)
+            while (GameRunning)
             {
                 yield return new WaitForSeconds(timeBetweenHeartbeats);
 
@@ -47,40 +47,24 @@ namespace Assets.Scripts.Game
                 eventHeartbeat.Invoke(_heartbeat);
                 Debug.Log($"[{GetType().Name}] Heartbeat {_heartbeat}");
                 _heartbeat++;
-
-                // Check win and lose conditions
-                //CheckWinCondition();
-                //CheckLoseCondition();
-
-                // Store the kings current position
-                //_previousKingPosition = _gridManager.king.Coordinates;
             }
         }
 
-        private void CheckWinCondition()
+        public void TriggerWinCondition()
         {
-            if (_gridManager.king.Coordinates == _gridManager.levelFinishTile.Coordinates)
-            {
-                _gameRunning = false;
-                OnLevelComplete();
-            }
+            GameRunning = false;
+            OnLevelComplete();
+        }
+
+        public void TriggerLoseCondition()
+        {
+            GameRunning = false;
+            OnGameLost();
         }
 
         private void OnLevelComplete()
         {
             Debug.Log("LEVEL IS WON");
-        }
-
-        private void CheckLoseCondition()
-        {
-            // Game not running anymore - probably because the win condition already triggered
-            if (!_gameRunning) return;
-
-            if (_gridManager.king.Coordinates == _previousKingPosition)
-            {
-                _gameRunning = false;
-                OnGameLost();
-            }
         }
 
         private void OnGameLost()
--- Assets/Scripts/Game/GridObjects/King.cs
+++ Assets/Scripts/Game/GridObjects/King.cs
@@ -6,9 +6,40 @@ namespace Assets.Scripts.Game.GridObjects
     {
         [SerializeField] private Vector2Int forwardVector;
 
+        private GameManager _gameManager;
+        private Vector2Int _previousCoordinates;
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            _gameManager = GameManager.Instance;
+        }
+
         protected override void OnHeartbeat(int heartbeat)
         {
+            _previousCoordinates = Coordinates;
             Move(forwardVector);
+            CheckWinCondition();
+            CheckLoseCondition();
+        }
+
+        private void CheckWinCondition()
+        {
+            if (Coordinates == gridManager.levelFinishTile.Coordinates)
+            {
+                _gameManager.TriggerWinCondition();
+            }
+        }
+
+        private void CheckLoseCondition()
+        {
+            // Game not running anymore - probably because the win condition already triggered
+            if (!_gameManager.GameRunning) return;
+
+            if (Coordinates == _previousCoordinates)
+            {
+                _gameManager.TriggerLoseCondition();
+            }
         }
     }
 }
